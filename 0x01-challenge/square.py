58cbc56d1ced52ecee06be2a3a8cffb4d646a4ef
Stephanie <Stephaniechloe21@gmail.com>
2023-09-19 17:44:41 -0700

    fixes 3

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Class Square"""
 
 
-class square():
+class Square():
     """Square Class"""
     width = 0
     height = 0
@@ -27,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
     """square object"""
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
